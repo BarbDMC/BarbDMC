9d187f543e19e2b67be455009e8d3a3c0f04ecc9
BarbDMC <barbaradmorantesc@gmail.com>
2022-03-25 22:44:31 -0500

    Improving sending email service

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template, request
-import smtplib
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = EMAIL_ADDRESS
+app.config['MAIL_PASSWORD'] = EMAIL_KEY
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+mail = Mail(app)
 
 @app.route('/', methods=['GET','POST'])
 
@@ -11,16 +19,16 @@ def index():
   email = request.form.get('email')
   message = request.form.get('message')
 
-  if not firstName and not lastName and not email and not message:
-    firstName = ''
-    lastName = ''
-    email = ''
-    message = ''
-  else:
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login("barbaradmorantesc@gmail.com", "rlutcgpusdzocadu")
-    server.sendmail("barbaradmorantesc@gmail.com", email, message)
+  if request.method == 'POST':
+    firstName = request.form.get('firstName')
+    lastName = request.form.get('lastName')
+    email = request.form.get('email')
+    message = request.form.get('message')
 
-
+    msg = Message('Contact from personal website', sender= (firstName + ' ' + lastName, email), recipients = [EMAIL_ADDRESS], reply_to=email)
+    msg.body = message
+    mail.send(msg)
+    flash('Message sent')
+    return redirect(url_for('index'))
+   
   return render_template("index.html")
