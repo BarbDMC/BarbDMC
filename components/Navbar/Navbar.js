9eb3af91a176ed0b27930df5f1afe5561478230b
BarbDMC <barbaradmorantesc@gmail.com>
2023-02-26 16:52:02 -0500

    [UPD-1] navbar elements refactored

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,31 +1,21 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import navbarInfo from './navbarInfo';
 
 const Navbar = () => {
   return (
    <nav>
     <ul>
-      <li>
-        <Link href='#about' passHref>
-          <a>About me</a >
-        </Link>
-      </li>
-      <li>
-        <Link href='#skills' passHref>
-          Work skills
-        </Link>
-      </li>
-      <li>
-        <Link href='#projects' passHref>
-          <a>Best projects</a>
-        </Link>
-      </li>
-      <li>
-        <Link href='#contact' passHref>
-          <a>Contact me</a>
-        </Link>
-      </li>
+      {
+        navbarInfo.map(({url, title}) => (
+          <li key={title}>
+            <Link href={url} passHref>
+              <a>{title}</a >
+            </Link>
+          </li>
+        ))
+      }
     </ul>
 
     <ul>
---
+++ components/Navbar/navbarInfo.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    url: '#about',
+    title: 'About me'
+  },
+  {
+    url: '#skills',
+    title: 'Work skills'
+  },
+  {
+    url: '#projects',
+    title: 'Best projects'
+  },
+  {
+    url: '#contact',
+    title: 'Contact me'
+  }
+];
