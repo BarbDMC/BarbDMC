62668fb53b00ef2091e087ca25f72178d8f2a3fb
Barbara Morantes <67979158+BarbDMC@users.noreply.github.com>
2023-03-03 14:14:40 -0500

    [UPD-1] Starting to work on hero section

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -3,14 +3,19 @@ import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <div>
-        <Image src="" alt="Barbara's profile image" />
+    <div className="text-center pt-16">
+      <Image className='rounded-full' src="/assets/images/avatar.jpeg" alt="Barbara's profile image" width={250} height={250} />
+      
+      <p className="mt-6 md:text-lg leading-8 text-sm">
+        I&apos;m a Full Stack developer that builds web app solutions that promote
+      </p>
 
-        <p>
-            I&apos;m a Full Stack developer that builds web app solutions that promote
-            inclusiveness and education because I want to enhance people&apos;s lives.
-        </p>
+      <p className="md:text-lg text-sm leading-8">
+        inclusiveness and education because I want to enhance people&apos;s lives.
+      </p>
+
     </div>
+   
   )
 }
 
