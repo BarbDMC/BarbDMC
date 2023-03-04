704628c233187e4f702f3fe23dc69fd46365d56b
BarbDMC <barbaradmorantesc@gmail.com>
2023-03-04 00:22:53 -0500

    [UPD-1] Hero section styles improved

--- components/About/AboutMe.js
+++ components/About/AboutMe.js
@@ -2,27 +2,27 @@ import React from 'react';
 
 const AboutMe = () => {
   return (
-    <section>
-      <h2>About me</h2>
+    <section className='sm:pt-60 pt-40 pb-40 text-center'>
+      <h2 className='text-3xl pb-10'>About me</h2>
 
-      <p>
+      <p className='leading-8 md:text-lg text-sm pl-6 pr-6 pb-5 text-justify md:text-center'>
         I&apos;m a Full Stack developer that wants to use technology to build useful
         solutions that will enhance people&apos;s lives by maximizing the effect that
         technology can provide.
       </p>
 
-      <p>
+      <p className='leading-8 md:text-lg text-sm pl-6 pr-6 pb-5 text-justify md:text-center'>
         I&apos;m a communicative, persistent, and self-taught person.
         who always stays up-to-date with industry trends in order 
         to provide better solutions.
       </p>
 
-      <p>
+      <p className='leading-8 md:text-lg text-sm pl-6 pr-6 pb-5 text-justify md:text-center'>
         Because of this, I&apos;ve been learning technologies like JavaScript, AngularJS, 
         NodeJS, and MongoDB in order to collaborate in the education sector.
       </p>
 
-      <p>
+      <p className='leading-8 md:text-lg text-sm pl-6 pr-6 text-justify md:text-center'>
         In addition, I want to keep developing myself since I want to contribute to fields
         like volunteering, E-learning, and accessibility.
       </p>
--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -3,17 +3,13 @@ import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <div className="text-center pt-16">
-      <Image className='rounded-full' src="/assets/images/avatar.jpeg" alt="Barbara's profile image" width={250} height={250} />
+    <div className="text-center sm:pt-16 pt-20">
+      <Image className='rounded-full' src="/assets/images/avatar.jpeg" alt="Barbara's profile image" width={300} height={300}  />
       
-      <p className="mt-6 md:text-lg leading-8 text-sm">
+      <p className="mt-6 md:text-lg md:text-center leading-8 text-sm pl-6 pr-6 text-justify">
         I&apos;m a Full Stack developer that builds web app solutions that promote
-      </p>
-
-      <p className="md:text-lg text-sm leading-8">
         inclusiveness and education because I want to enhance people&apos;s lives.
       </p>
-
     </div>
    
   )
