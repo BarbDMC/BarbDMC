b123cf06f07b701df77c2b92dc5fb2c864a98e26
Barbara Morantes <67979158+BarbDMC@users.noreply.github.com>
2023-03-08 17:27:19 -0500

    [UPD-1] Skills section desktop version completed

--- components/Skills/Skills.js
+++ components/Skills/Skills.js
@@ -1,28 +1,30 @@
 import React from 'react';
 import skillsInfo from './skillsInfo';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const Skills = () => {
   return (
-    <section>
-      <h3>Work skills</h3>
+    <section id='skills' className='py-32'>
+      <h3 className="mb-2 text-2xl font-bold text-center tracking-tight text-gray-900 dark:text-white">Work skills</h3>
 
-      {
-        skillsInfo.map((skill) => (
-          <div key={skill.title}>
-            <i className={skill.icon}></i>
-            <h3>{skill.title}</h3>
+      <div className='flex justify-evenly pt-14 px-24'>
+        {
+          skillsInfo.map((skill) => (
+            <div key={skill.title} className='border rounded-lg py-5 px-5 w-52 text-center'>
+              <FontAwesomeIcon className='mb-2' icon={skill.icon} style={{ fontSize: 30, color: 'black' }}/>
+              <h3 className="mb-4 text-2xl font-bold text-center tracking-tight text-gray-900 dark:text-white">{skill.title}</h3>
 
-            <ul>
-            {
-              skill.list.map((skill) => (
-                <li key={skill}>{skill}</li>
-              ))
-            }
-            </ul>
-          </div>
-        ))
-      }
-
+              <ul className="list-disc ml-5">
+              {
+                skill.list.map((skill) => (
+                  <li className='text-left text-md' key={skill}>{skill}</li>
+                ))
+              }
+              </ul>
+            </div>
+          ))
+        }
+      </div>
     </section>
   )
 }
