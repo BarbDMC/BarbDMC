f370d2491fad439d786c34e60e50fa1a1a5103f6
BarbDMC <barbaradmorantesc@gmail.com>
2023-11-19 16:25:20 -0500

    Footer styles added

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import SocialMediaList from '../socialMediaList/socialMediaList';
 
 const Footer = () => {
   return (
-    <div className='w-full flex justify-around'>
-      <SocialMediaList />
-
-      <span className='inline-flex items-center text-xs md:text-xl'>© {new Date().getFullYear()} | Designed by Barbara Morantes</span>
+    <div className='bg-[rgba(212,236,255,1)] w-full h-[124px] flex justify-around'>
+      <span className='[font-family:Signika_Negative] text-[28px] flex col items-center font-bold text-[rgba(41,55,74,1)] max-w-[480px] grow-0 shrink-0 basis-auto box-border'>© {new Date().getFullYear()} | Designed by Barbara Morantes</span>
     </div>
   )
 }
