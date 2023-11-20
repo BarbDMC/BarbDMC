fee4e29bd8b2ab6bf9aa6da032b21e39dd4f0f1c
BarbDMC <barbaradmorantesc@gmail.com>
2023-11-19 21:02:04 -0500

    Changing projects section

--- components/Projects/Projects.js
+++ components/Projects/Projects.js
@@ -6,50 +6,30 @@ import starProjectInfo from './starProjectInfo';
 
 const Projects = () => {
   return (
-    <section id='projects' className='pt-32'>
-      <h2 className='text-center font-bold text-3xl' >Best Projects</h2>   
+    <div
+      className={`mx-32 my-[140px] shadow-[4px_4px_25px_4px_rgba(0,0,0,0.25)] [background:linear-gradient(180.00deg,rgba(199,227,255,0.85)_22.97%,rgba(249,138,170,0.85)_112.07%)] flex justify-start items-center flex-col min-w-[1367px] box-border pl-[69px] pr-[73px] pt-[49px] pb-20 rounded-br-[40px] rounded-t-[40px] rounded-bl-[40px]`}
+    >
+           <p className={`[font-family:Signika_Negative] text-[40px] font-bold text-center text-[rgba(41,55,74,1)] self-stretch grow-0 shrink-0 basis-auto`}>Best Projects</p>
 
-     <div className='pt-14 md:px-24 px-6'>
+        <div className={`flex justify-start items-center flex-col gap-[39px] grow-0 shrink-0 basis-auto pl-[59px] pr-[38px]`}>
         {starProjectInfo.map((info) => (
-          <div key={info} className="md:w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-              <Image src={info.imageSrc} 
-                          alt={info.altText}
-                          width={1250} height={250}
-                          className="rounded-t-lg"
-                />
-              <div className="p-5">
-                  <h3 className="mb-2 text-2xl font-bold text-center tracking-tight text-gray-900 dark:text-white">{info.title}</h3>
-                  <p className="mb-3 font-normal text-justify dark:text-gray-400">{info.description}</p>
+           <div key={info}>
+              <Image src={info.imageSrc}
+            alt={info.altText}
+            width={1250} height={250}
+            className="`object-cover [border-top-style:none] [border-right-style:none] [border-bottom-style:none] [border-left-style:none] w-[1128px] h-[130px] max-w-[initial] box-border block self-stretch grow-0 shrink-0 basis-auto rounded-br-[31.5px] rounded-t-[31.5px] rounded-bl-[31.5px]"
+              />
 
-                  <div className="w-full font-medium rounded-lg text-sm px-5 pt-3 text-center">
-                      <a href={info.url} alt={info.altText} className='px-3 py-2 text-md font-bold border rounded-lg dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800' target="_blank" rel="noreferrer">See this project</a>
-                  </div>
-              </div>
+          <p className={`[font-family:Signika_Negative] text-[32px] font-bold text-center text-[rgba(41,55,74,1)] mt-[-3px] self-stretch grow-0 shrink-0 basis-auto`}>
+            {info.title}
+          </p>
            </div>
         ))}
-     </div>
-
-      
-     <div className='flex pt-14 md:px-24 px-6'>
-      {projectsInfo.map((info, index) => (
-           <div key={info} className={"md:w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" + (index === 0 ? ' md:mr-8 mr-2' : '')}>
-              <Image src={info.imageSrc} 
-                          alt={info.altText}
-                          width={625} height={250}
-                          className="rounded-t-lg"
-                />
-              <div className="md:p-5 pb-5 px-2">
-                  <h3 className="mb-2 md:text-2xl text-sm font-bold text-center tracking-tight text-gray-900 dark:text-white">{info.title}</h3>
-                  <p className="mb-3 font-normal text-justify text-sm dark:text-gray-400">{info.description}</p>
-
-                  <div className="md:w-full font-medium rounded-lg text-sm md:px-5 px-0 pt-3 text-center">
-                      <a href={info.url} alt={info.altText} className='px-3 py-2 md:text-md text-xs font-bold border rounded-lg dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800' target="_blank" rel="noreferrer">See this project</a>
-                  </div>
-              </div>
         </div>
-        ))}
-     </div>
-    </section>
+
+      <div className={`flex justify-start items-stretch flex-row self-stretch grow-0 shrink-0 basis-auto mt-20`}>
+      </div>
+    </div>
   )
 }
 
--- components/Projects/starProjectInfo.js
+++ components/Projects/starProjectInfo.js
@@ -1,10 +1,10 @@
 
 module.exports = [
     {
-      imageSrc: '/assets/images/inclusion.webp',
-      altText: 'Inclusion at work project\'s image',
-      title: 'Inclusion at work (In progress)',
-      description: 'The inclusion at-work system is going to be a web app focused on covering the needs of the company\'s employees that need to report incidents of workplace harassment that may occur by discrimination.',
-      url: 'https://magnificent-radar-b6d.notion.site/Inclusion-at-work-c14bdebbe29e451d9693c3445ee3105e'
+      imageSrc: '/assets/images/pico&placa.png',
+      altText: 'Pico and Placa project\'s image',
+      title: 'Pico y Placa Predictor',
+      description: 'The project allows users to consult if whether or not their car can be on the road based on their license plate number. The project was done with JavaScript, NodeJs, and Express.js.',
+      url: ''
     }
 ];
