132373e4d6b39ed2875a01d3eff2fff4d889bb9a
BarbDAMC <barbara.morantes@thoughtworks.com>
2026-04-17 20:58:27 -0500

    UPD-1: Update footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className='bg-[rgba(212,236,255,1)] w-full h-[124px] flex justify-around'>
-      <span className='[font-family:Signika_Negative] text-[28px] flex col items-center font-bold text-[rgba(41,55,74,1)] max-w-[480px] grow-0 shrink-0 basis-auto box-border'>© {new Date().getFullYear()} | Designed by Barbara Morantes</span>
+    <div className='w-full py-6 flex justify-center items-center rounded-2xl' style={{backgroundColor: 'rgba(97, 184, 255, 0.8)'}}>
+      <span className='text-white text-center font-bold text-sm md:text-base'>© {new Date().getFullYear()} | Designed by Barbara Morantes</span>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
     <AboutMe />
     <Projects />
     <Skills />
-    {/* <Footer /> */}
+    <Footer />
    </div>
   )
 }
