191dfad3eb1de8aeff3342cb92d25cab18c7cd8a
BarbDAMC <barbara.morantes@thoughtworks.com>
2026-04-17 20:02:39 -0500

    UPD-1: Update About me section

--- components/About/AboutMe.js
+++ components/About/AboutMe.js
@@ -1,29 +1,49 @@
 import React from 'react';
+import { Nunito } from 'next/font/google';
+
+const nunito = Nunito({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  display: 'swap',
+});
 
 const AboutMe = () => {
   return (
-    <section id='about'
-      className='md:mx-32 md:my-36 mx-8 my-20 md:py-11 md:px-14 py-8 px-10 shadow-[4px_4px_25px_4px_rgba(26,146,255,0.25)] bg-white flex justify-start items-stretch flex-col grow-0 shrink-0 basis-auto rounded-br-[40px] rounded-t-[40px] rounded-bl-[40px]'
+    <section
+      id="about"
+      className={`${nunito.className} bg-white px-4 py-16 sm:px-6 sm:py-20 md:px-10 md:py-24 lg:py-28`}
+      aria-labelledby="about-heading"
     >
-      <p className='[font-family:Signika_Negative] md:text-4xl text-2xl font-bold text-center text-[rgba(4,49,76,1)] grow-0 shrink-0 basis-auto'>About me</p>
-      <div className='[font-family:Nunito] md:text-2xl text-base font-normal text-center leading-10 text-[rgba(4,49,76,1)] grow-0 shrink-0 basis-auto mt-[47px]'>
-        <p className='leading-8 whitespace-break-spaces'>
-          I&apos;m a Full Stack developer that wants to use technology to build useful solutions that will enhance people&apos;s lives by
-        </p>
-        <p className='mb-10'>maximizing the effect that technology can provide.</p>
+      <div className="mx-auto flex w-full max-w-3xl justify-center">
+        <div className="w-full rounded-[2rem] bg-white px-8 py-11 shadow-[0_8px_40px_rgba(26,146,255,0.28),0_4px_16px_rgba(26,146,255,0.12)] sm:rounded-[2.25rem] sm:px-10 sm:py-12 md:px-14 md:py-14 lg:px-16 lg:py-16">
+          <h2
+            id="about-heading"
+            className="text-center text-2xl font-bold tracking-tight text-[#0c2744] sm:text-3xl md:text-4xl"
+          >
+            About me
+          </h2>
 
-        <p className='leading-8 mb-10'>
-          Because of this, I&apos;ve been learning technologies like JavaScript, AngularJS, NodeJS, and MongoDB in order to 
-          <br className='md:block hidden' /> collaborate in the education sector.
-        </p>
+          <div className="mt-8 space-y-6 text-center text-[0.95rem] font-normal leading-relaxed text-[#0c2744] sm:text-base md:mt-10 md:space-y-7 md:text-lg md:leading-8">
+            <p>
+              I&apos;m a Full Stack developer that wants to use technology to build useful solutions
+              that will enhance people&apos;s lives by maximizing the effect that technology can
+              provide.
+            </p>
 
-        <p className='leading-8 mb-10'>
-          In addition, I want to keep developing myself since I want to contribute to fields like volunteering,
-          <br className='md:block hidden' /> E-learning, and accessibility.
-        </p>
+            <p>
+              Because of this, I&apos;ve been learning technologies like JavaScript, AngularJS,
+              NodeJS, and MongoDB in order to collaborate in the education sector.
+            </p>
+
+            <p>
+              In addition, I want to keep developing myself since I want to contribute to fields like
+              volunteering, E-learning, and accessibility.
+            </p>
+          </div>
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default AboutMe
+export default AboutMe;
